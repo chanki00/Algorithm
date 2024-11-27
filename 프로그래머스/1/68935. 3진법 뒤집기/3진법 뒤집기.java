14b93544eef50a34c249f1265a900c2a61e8bac8
chanki <115077587+chanki00@users.noreply.github.com>
2024-11-27 09:37:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append(n % 3);
+            n /= 3;
+        }
+        
+        String s = sb.toString();
+        
+        for (int i=0; i<s.length(); ++i) {
+            answer += Integer.parseInt(String.valueOf(s.charAt(i))) * Math.pow(3, s.length()-i-1);
+        }
+        
+        return answer;
+    }
+}
