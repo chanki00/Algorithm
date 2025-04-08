4e110ba7ec7569cf30fa7639f1d010807d07b9dc
chanki <115077587+chanki00@users.noreply.github.com>
2025-04-08 11:00:48 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 452 ms, Memory: 125512 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int[] arr, tmp;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i=0; i<N; ++i) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int idx = 1;
+		tmp = new int[N];
+		tmp[0] = arr[0];
+		for (int i=1; i<N; ++i) {
+			if (arr[i] > tmp[idx-1]) {
+				tmp[idx] = arr[i];
+				++idx;
+			}
+			else if (arr[i] < tmp[idx-1]) { // 들어갈 곳 찾기
+				tmp[getIdx(0, idx, arr[i])] = arr[i];
+			}
+		}
+		
+//		System.out.println(Arrays.toString(tmp));
+		System.out.println(idx);
+	}
+	
+	public static int getIdx(int lo, int hi, int val) {
+		
+		while (lo < hi) {
+			int mid = (lo + hi) / 2;
+			
+			if (tmp[mid] >= val) {
+				hi = mid;
+			}
+			else {
+				lo = mid+1;
+			}
+		}
+		
+		return lo;
+	}
+}
