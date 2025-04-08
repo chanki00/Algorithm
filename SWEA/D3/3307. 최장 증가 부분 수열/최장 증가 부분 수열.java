1fa62113695d850d766d1c16cb3def0e101d1e82
chanki <115077587+chanki00@users.noreply.github.com>
2025-04-08 15:25:30 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 98 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,42 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int[] arr, tmp;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int T = Integer.parseInt(br.readLine());
-
 		for (int t = 1; t <= T; ++t) {
-
 			int N = Integer.parseInt(br.readLine());
+			arr = new int[N];
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] arr = new int[N];
 			for (int i = 0; i < N; ++i) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 
-			int[] memo = new int[N];
-			memo[0] = 1;
+			int idx = 1;
+			tmp = new int[N];
+			tmp[0] = arr[0];
 			for (int i = 1; i < N; ++i) {
-				int maxMemo = 0;
-				for (int j = 0; j < i; ++j) {
-					if (arr[j] < arr[i]) {
-						if (memo[j] > maxMemo) {
-							maxMemo = memo[j];
-						}
-					}
+				if (arr[i] > tmp[idx - 1]) {
+					tmp[idx] = arr[i];
+					++idx;
+				} else if (arr[i] < tmp[idx - 1]) { // 들어갈 곳 찾기
+					tmp[getIdx(0, idx, arr[i])] = arr[i];
 				}
-				memo[i] = maxMemo + 1;
 			}
 
-//		System.out.println(Arrays.toString(memo));
-			Arrays.sort(memo);
+//			System.out.println(Arrays.toString(tmp));
 			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(t).append(" ").append(memo[N-1]);
+			sb.append("#").append(t).append(" ").append(idx);
 			System.out.println(sb);
 		}
+	}
+
+	public static int getIdx(int lo, int hi, int val) {
+
+		while (lo < hi) {
+			int mid = (lo + hi) / 2;
+
+			if (tmp[mid] >= val) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+
+		return lo;
 	}
 }
