1d7e81ccc79686504ac985ae4e6c3f26214311b7
chanki <115077587+chanki00@users.noreply.github.com>
2025-08-01 08:58:28 +0900

    [Silver III] Title: N번째 큰 수, Time: 1604 ms, Memory: 338056 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<N; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; ++j) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        System.out.println(list.get(N-1));
+    }
+}
