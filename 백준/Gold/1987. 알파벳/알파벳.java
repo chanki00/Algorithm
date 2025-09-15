ea084b6369ce7b9c7b845954088bdb0aebb05125
chanki <115077587+chanki00@users.noreply.github.com>
2025-09-15 23:25:24 +0900

    [Gold IV] Title: 알파벳, Time: 912 ms, Memory: 12516 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,59 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int[][] unit = {
-			{1, 0}, {-1,0}, {0, 1}, {0, -1}
-	};
-	
-	static int max = 1;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		char[][] map = new char[R][C];
-		for (int i=0;i<R; ++i) {
-			map[i] = br.readLine().toCharArray();
-		}
-		
-		boolean[] visited = new boolean[26];
-		int currR = 0;
-		int currC = 0;
-	
-		visited[map[currR][currC] - 65] = true;
 
-		solve(map, visited, 0, 0, max);
-		
-		System.out.println(max);
-	}
-	
-	public static void solve(char[][] map, boolean[] visited, int currR, int currC, int depth) {
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    static boolean[] alpha = new boolean[26];
+    static char[][] map;
+    static int R, C, max;
 
-		max = Math.max(max,  depth);
-		
-		for (int i=0; i<4; ++i) {
-			int nextR = currR + unit[i][0];
-			int nextC = currC + unit[i][1];
-			
-			if (isValid(map, visited, nextR, nextC)) {
-				visited[map[nextR][nextC] - 65] = true;
-				solve(map, visited, nextR, nextC, depth + 1);
-				visited[map[nextR][nextC] - 65] = false;
-			}
-		}
-		
-	}
-	
-	public static boolean isValid(char[][] map, boolean[] visited, int r, int c) {
-		return r >= 0 && c >= 0 && r < map.length && c < map[0].length && !visited[map[r][c]-65];
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        max = Integer.MIN_VALUE;
 
+        map = new char[R][C];
+        for (int i=0; i<R; ++i) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        alpha[map[0][0]-'A'] = true;
+        dfs(0, 0, 1);
+
+        System.out.println(max);
+    }
+
+    public static void dfs(int r, int c, int cnt) {
+        int idx = map[r][c] - 'A';
+
+        max = Math.max(max, cnt);
+
+        for (int d=0; d<4; ++d) {
+            int nextR = r + dr[d];
+            int nextC = c + dc[d];
+            if (isValid(nextR, nextC) && !alpha[map[nextR][nextC] - 'A']) {
+                alpha[map[nextR][nextC] - 'A'] = true;
+                dfs(nextR, nextC, cnt+1);
+                alpha[map[nextR][nextC] - 'A'] = false;
+            }
+        }
+    }
+
+    public static boolean isValid(int r, int c) {
+        return r >= 0 && r < map.length && c >= 0 && c < map[r].length;
+    }
 }
