4b8bdb562a7b9b31a09db34c69c60af11cdc94ec
chanki <115077587+chanki00@users.noreply.github.com>
2025-11-25 23:56:04 +0900

    [Gold III] Title: 줄 세우기, Time: 328 ms, Memory: 45988 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,84 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * 위상 정렬? -> 차수가 0인 애부터 출력
- */
 public class Main {
-	static class Node {
-		int n;
-		int d;
-		
-		Node(int n, int d) {
-			this.n = n;
-			this.d = d;
-		}
-	}
-	
-	static List<List<Integer>> graph;
-	static int[] degree;
-	static Queue<Integer> q;	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); // 정점 수
-		int M = Integer.parseInt(st.nextToken()); // 간선 수
-		
-		graph = new ArrayList<>();
-		q = new ArrayDeque<>();
-		
-		for (int i=0; i<=N; ++i) {
-			graph.add(new ArrayList<>());
-		}
-		
-		degree = new int[N+1];
-		for (int i=0; i<M; ++i) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			++degree[e];
-			graph.get(s).add(e);
-		}
-		
-		for (int i=1; i<degree.length; ++i) {
-			if (degree[i] == 0) {
-				q.add(i);
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		find(sb);
-		
-		for (int i=1; i<degree.length; ++i) {
-			if (degree[i] != -1) {
-				sb.append(i).append(" ");
-			}
-		}
-		
-		System.out.println(sb);
-	}
-	
-	public static void find(StringBuilder sb) {
-		while (!q.isEmpty()) {
-			int curr = q.poll();
-			sb.append(curr).append(" ");
-			degree[curr] = -1;
-			
-			List<Integer> list = graph.get(curr);
-			for (int i=0; i<list.size(); ++i) {
-				--degree[list.get(i)];
-				if (degree[list.get(i)] == 0) {
-					q.add(list.get(i));
-				}
-			}
-		}
-	}
+
+    static class Node {
+        int n;
+        int d;
+
+        public Node(int n, int d) {
+            this.n = n;
+            this.d = d;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i=0; i<=N; ++i) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] degree = new int[N+1];
+
+        for (int i=0; i<M; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            ++degree[e];
+            graph.get(s).add(e);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i=1; i<degree.length; ++i) {
+            if (degree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            sb.append(curr).append(" ");
+            --degree[curr];
+
+            for (int next : graph.get(curr)) {
+                --degree[next];
+                if (degree[next] == 0) {
+                    q.add(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
