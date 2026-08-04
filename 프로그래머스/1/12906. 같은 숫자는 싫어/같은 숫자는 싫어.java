9ec64610562525db7c96a74fef35dcf8eada9a31
chanki <115077587+chanki00@users.noreply.github.com>
2026-08-04 21:28:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.44 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,23 +1,19 @@
+import java.io.*;
 import java.util.*;
 
-public class Solution {
-    public int[] solution(int []arr) {
-        int[] answer;
 
-        List<Integer> list = new ArrayList<>();
-        
+public class Solution {
+    public List<Integer> solution(int []arr) {
+        List<Integer> answer = new ArrayList<>();
+
         int prev = -1;
-        for (int i=0; i<arr.length; ++i) {
-            if (prev != arr[i]) {
-                prev = arr[i];
-                list.add(prev);
+        for (int a : arr) {
+            if (a != prev) {
+                answer.add(a);
             }
+            prev = a;
         }
         
-        answer = new int[list.size()];
-        for (int i=0; i<list.size(); ++i) {
-            answer[i] = list.get(i);
-        }
 
         return answer;
     }
