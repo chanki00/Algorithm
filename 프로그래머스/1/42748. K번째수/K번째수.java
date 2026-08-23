9cef60f11adaa5a91774b106b2486849901d33dd
chanki <115077587+chanki00@users.noreply.github.com>
2026-08-23 20:34:25 +0900

    [level 1] Title: K번째수, Time: 0.16 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        List<Integer> list;
+        
+        for (int i=0; i<commands.length; ++i) {
+            list = new ArrayList<>();
+            for (int j=commands[i][0]-1; j<commands[i][1]; ++j) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2]-1);
+        }
+        
+        
+        return answer;
+    }
+}
