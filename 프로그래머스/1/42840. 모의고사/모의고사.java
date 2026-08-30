c7ef189747c38e00ef0a54e171f2316e511bd4fe
chanki <115077587+chanki00@users.noreply.github.com>
2026-08-30 21:30:12 +0900

    [level 1] Title: 모의고사, Time: 0.69 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,44 +1,30 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] answer;
-        int[][] people = new int[][] {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-        Map<Integer, Integer> map = new HashMap<>() {{
-            put(1, 0);
-            put(2, 0);
-            put(3, 0);
-        }};
-        
-        for (int i=0; i<answers.length; ++i) {
-            for (int j=0; j<3; ++j) {
-                if (answers[i] == people[j][i%people[j].length]) {
-                    map.put(j+1, map.get(j+1)+1);
-                }
+    public static int[] solution(int[] answers) {
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
             }
         }
-        
-        int max = Integer.MIN_VALUE;
-        for (int value : map.values()) {
-            if (value > max) {
-                max = value;
-            }
-        }
-        
+
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
         List<Integer> list = new ArrayList<>();
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                list.add(key);
-            }
-        }
-        
-        answer = new int[list.size()];
-        for (int i=0; i<answer.length; ++i) {
-            answer[i] = list.get(i);
-        }
-        
-        Arrays.sort(answer);
-        
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
+
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
         return answer;
     }
 }
